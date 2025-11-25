4c91d0b42bc1bf6943961787d8437ac66e91993a
juuxi <dimashopyrev@mail.ru>
2025-11-25 19:57:51 +0300

    routing for comments/posts fixed

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -7,14 +7,14 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:id>/', views.post_detail, name='post_detail'),
+    path('posts/<int:id>/', views.post_detail, name='post_detail'),
     path('profile/edit/', views.ProfileUpdateView.as_view(), name='edit_profile'),
     path('profile/<str:username>/', views.view_profile, name='profile'),
-    path('create/', views.create_post, name='create_post'),
-    path('<int:id>/edit/', views.edit_post, name='edit_post'),
-    path('<int:pk>/delete/', views.PostDeleteView.as_view(), name='delete_post'),
-    path('<int:post_id>/comment/', views.add_comment, name='add_comment'),
-    path('<int:post_id>/edit_comment/<int:comment_id>/', views.edit_comment, name='edit_comment'),
-    path('<int:post_id>/delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
+    path('posts/create/', views.create_post, name='create_post'),
+    path('posts/<int:id>/edit/', views.edit_post, name='edit_post'),
+    path('posts/<int:id>/delete/', views.PostDeleteView.as_view(), name='delete_post'),
+    path('posts/<int:post_id>/comment/', views.add_comment, name='add_comment'),
+    path('posts/<int:post_id>/edit_comment/<int:comment_id>/', views.edit_comment, name='edit_comment'),
+    path('posts/<int:post_id>/delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
     path('<slug:category_slug>/', views.category_posts, name='category_posts'),
 ]
