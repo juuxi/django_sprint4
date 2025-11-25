94e4ae9ab0275bd02f74be7889e0200c41943624
juuxi <dimashopyrev@mail.ru>
2025-11-25 19:09:55 +0300

    routing fix

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     path('create/', views.create_post, name='create_post'),
     path('<int:id>/edit/', views.edit_post, name='edit_post'),
     path('<int:pk>/delete/', views.PostDeleteView.as_view(), name='delete_post'),
-    path('', views.edit_profile, name='edit_profile'),
+    path('profile/<str:username>/edit/', views.edit_profile, name='edit_profile'),
     path('<int:post_id>/comment/', views.add_comment, name='add_comment'),
-    path('<int:post_id>/edit_comment/<int:comment_id>', views.edit_comment, name='edit_comment'),
-    path('<int:post_id>/delete_comment/<int:comment_id>', views.delete_comment, name='delete_comment'),
+    path('<int:post_id>/edit_comment/<int:comment_id>/', views.edit_comment, name='edit_comment'),
+    path('<int:post_id>/delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
     path('<slug:category_slug>/', views.category_posts, name='category_posts'),
 ]
