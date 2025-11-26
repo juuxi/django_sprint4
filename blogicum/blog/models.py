085f9a9cfcaa5141344587cc21d9ea2e0bd65b6d
juuxi <dimashopyrev@mail.ru>
2025-11-26 16:32:43 +0300

    TimeManagementAbstractModel

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -5,19 +5,25 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class Category(models.Model):
-    title = models.CharField(max_length=256, verbose_name='Заголовок')
-    description = models.TextField(verbose_name='Описание')
-    slug = models.SlugField(unique=True, verbose_name='Идентификатор',
-                            help_text='Идентификатор страницы для URL; '
-                            'разрешены символы латиницы, цифры, '
-                            'дефис и подчёркивание.')
+class TimeManagementAbstractModel(models.Model):
     is_published = models.BooleanField(default=True,
                                        verbose_name='Опубликовано',
                                        help_text='Снимите галочку, '
                                        'чтобы скрыть публикацию.')
     created_at = models.DateTimeField(auto_now_add=True,
                                       verbose_name='Добавлено')
+
+    class Meta:
+        abstract = True
+
+
+class Category(TimeManagementAbstractModel):
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
+    description = models.TextField(verbose_name='Описание')
+    slug = models.SlugField(unique=True, verbose_name='Идентификатор',
+                            help_text='Идентификатор страницы для URL; '
+                            'разрешены символы латиницы, цифры, '
+                            'дефис и подчёркивание.')
 
     class Meta:
         verbose_name = 'категория'
@@ -27,14 +33,8 @@ class Category(models.Model):
         return self.title
 
 
-class Location(models.Model):
+class Location(TimeManagementAbstractModel):
     name = models.CharField(max_length=256, verbose_name='Название места')
-    is_published = models.BooleanField(default=True,
-                                       verbose_name='Опубликовано',
-                                       help_text='Снимите галочку, '
-                                       'чтобы скрыть публикацию.')
-    created_at = models.DateTimeField(auto_now_add=True,
-                                      verbose_name='Добавлено')
 
     class Meta:
         verbose_name = 'местоположение'
@@ -44,7 +44,7 @@ class Location(models.Model):
         return self.name
 
 
-class Post(models.Model):
+class Post(TimeManagementAbstractModel):
     title = models.CharField(max_length=256, verbose_name='Заголовок')
     text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField(verbose_name='Дата и время публикации',
@@ -68,12 +68,6 @@ class Post(models.Model):
         null=True,
         verbose_name='Категория'
     )
-    is_published = models.BooleanField(default=True,
-                                       verbose_name='Опубликовано',
-                                       help_text='Снимите галочку, '
-                                       'чтобы скрыть публикацию.')
-    created_at = models.DateTimeField(auto_now_add=True,
-                                      verbose_name='Добавлено')
 
     image = models.ImageField('Фото', upload_to='post_images', blank=True)
 
